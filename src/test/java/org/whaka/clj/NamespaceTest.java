38121becc21083d4990a4a307aebafa0edf1de49
Ruslan Dudin <rdudin@devexperts.com>
2016-07-13 11:00:55 +0300

    #1 Implemet namespace loading
    1. Git-ignore added
    2. Name-space example changed

--- src/test/java/org/whaka/clj/NamespaceTest.java
+++ src/test/java/org/whaka/clj/NamespaceTest.java
@@ -1,17 +1,12 @@
 package org.whaka.clj;
 
-import clojure.java.api.Clojure;
-import clojure.lang.IFn;
-
 public class NamespaceTest {
 
 	public static void main(String[] args) throws Exception {
 		
-		IFn require = Clojure.var("clojure.core", "require");
-		IFn deref = Clojure.var("clojure.core", "deref");
-		require.invoke(Clojure.read("org.whaka.clj.test-ns"));
-		
-		IFn var = Clojure.var("org.whaka.clj.test-ns", "test-var");
-		System.out.println(deref.invoke(var));
+		String ns = "org.whaka.clj.test-ns";
+		UberClj.require(ns);
+		System.out.println(UberClj.fn(ns, "test-fn").invoke(303));
+		System.out.println(UberClj.value(ns, "test-var"));
 	}
 }
