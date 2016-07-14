839ed0f9f711d72ef67e52739ad22be5cf8e4a23
Ruslan Dudin <rdudin@devexperts.com>
2016-07-14 13:44:25 +0300

    #5 Clojure entities factory methods
    1. Collection factories implemented in `UberClj`
    2. Javadocs fixed a bit

--- src/main/java/org/whaka/clj/UberClj.java
+++ src/main/java/org/whaka/clj/UberClj.java
@@ -1,9 +1,17 @@
 package org.whaka.clj;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
+import clojure.lang.IPersistentList;
+import clojure.lang.ISeq;
+import clojure.lang.PersistentHashMap;
+import clojure.lang.PersistentHashSet;
+import clojure.lang.PersistentList;
+import clojure.lang.PersistentVector;
 import clojure.lang.Var;
 
 /**
@@ -64,5 +72,113 @@ public final class UberClj {
 	 */
 	public static IFn fn(String ns, String name) {
 		return UberClj.<IFn>value(ns, name);
+	}
+	
+	/**
+	 * Create a persistent vector from the specified elements
+	 * 
+	 * @see #vector(Collection)
+	 */
+	public static PersistentVector vector(Object... elements) {
+		return PersistentVector.create(elements);
+	}
+	
+	/**
+	 * Create a persistent vector from the specified elements
+	 * 
+	 * @see #vector(Object...)
+	 */
+	public static PersistentVector vector(Collection<?> elements) {
+		return PersistentVector.create(elements);
+	}
+	
+	/**
+	 * Create a persistent list from the specified elements
+	 * 
+	 * @see #list(Collection)
+	 */
+	public static IPersistentList list(Object... elements) {
+		return PersistentList.create(vector(elements));
+	}
+	
+	/**
+	 * <p>Create a persistent list from the specified elements
+	 * 
+	 * <p><b>Note:</b> the most direct construction will be performed by this method
+	 * if you pass it an instance of {@link List}. Otherwise - list will be constructed thru a vector.
+	 * 
+	 * @see #list(Object...)
+	 */
+	public static IPersistentList list(Collection<?> elements) {
+		return PersistentList.create(
+				elements instanceof List ?
+						(List<?>) elements : vector(elements));
+	}
+	
+	/**
+	 * Create a persistent set (removed duplicated elements)
+	 * 
+	 * @see #set(Collection)
+	 */
+	public static PersistentHashSet set(Object... elements) {
+		return PersistentHashSet.create(elements);
+	}
+	
+	/**
+	 * Create a persistent set (removed duplicated elements)
+	 * 
+	 * @see #set(Object...)
+	 */
+	public static PersistentHashSet set(Collection<?> elements) {
+		return PersistentHashSet.create(elements);
+	}
+	
+	/**
+	 * Create a persistent map from the even number of the specified elements,
+	 * where each first element is a key, and each second element is a value.
+	 * 
+	 * @throws IllegalArgumentException in case odd number of elements is passed
+	 * 
+	 * @see #map(Collection)
+	 */
+	public static PersistentHashMap map(Object... elements) {
+		assertMapSize(elements.length);
+		return PersistentHashMap.create(elements);
+	}
+	
+	/**
+	 * Create a persistent map from the even number of the specified elements,
+	 * where each first element is a key, and each second element is a value.
+	 * 
+	 * @throws IllegalArgumentException in case odd number of elements is passed
+	 * 
+	 * @see #map(Object...)
+	 */
+	public static PersistentHashMap map(Collection<?> elements) {
+		assertMapSize(elements.size());
+		return PersistentHashMap.create(elements);
+	}
+	
+	private static void assertMapSize(int size) {
+		if ((size & 1) > 0)
+			throw new IllegalArgumentException("Map may be constructed only from even number of elements!");
+	}
+	
+	/**
+	 * Create an abstract seq from the specified elements
+	 * 
+	 * @see #seq(Collection)
+	 */
+	public static ISeq seq(Object... elements) {
+		return vector(elements).seq();
+	}
+	
+	/**
+	 * Create an abstract seq from the specified elements
+	 * 
+	 * @see #seq(Object...)
+	 */
+	public static ISeq seq(Collection<?> elements) {
+		return vector(elements).seq();
 	}
 }
